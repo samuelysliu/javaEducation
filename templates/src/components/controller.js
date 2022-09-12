d8bf23dc3540bddc5ade827403d8fad4e3cfa165
samuelbarry <samuel870822@gmail.com>
2022-09-12 18:35:22 +0800

    change main address

--- templates/src/components/controller.js
+++ templates/src/components/controller.js
@@ -3,7 +3,7 @@ export class Controller{
         if(process.env.REACT_APP_NETWORK === "local"){
             this.apiPath = "http://127.0.0.1:5000"
         }else if(process.env.REACT_APP_NETWORK === "main"){
-            this.apiPath = "https://three-step-coding.fly.dev"
+            this.apiPath = "http://163.14.74.159:5000"
         }else{
             this.apiPath = "http://127.0.0.1:5000"
         }
