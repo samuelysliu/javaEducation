b0f50c4cf6c3ec1618b21a97778a33662beae204
logicofgod <dapp.prize@gmail.com>
2022-09-23 10:56:48 +0800

    change setting name

--- templates/src/components/controller.js
+++ templates/src/components/controller.js
@@ -1,8 +1,8 @@
 export class Controller{
     constructor(){
-        if(process.env.REACT_APP_NETWORK === "local"){
+        if(process.env.REACT_APP_Environment === "local"){
             this.apiPath = "http://127.0.0.1:5000"
-        }else if(process.env.REACT_APP_NETWORK === "main"){
+        }else if(process.env.REACT_APP_Environment === "main"){
             this.apiPath = "http://163.14.74.159:5000"
         }else{
             this.apiPath = "http://127.0.0.1:5000"
