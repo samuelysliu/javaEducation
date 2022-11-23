ed082a6478681184d5c3166024f5b70af5b8f83f
logicofgod <dapp.prize@gmail.com>
2022-11-23 16:59:16 +0800

    close group try

--- control/groupControl.py
+++ control/groupControl.py
@@ -8,6 +8,65 @@ userDB = userInfo()
 
 
 def grouping(file, className):
+    data = pd.read_excel(file, header=0, converters={'identity': str, 'grades': int})
+    dataString = data.to_json()
+    # let data type as json
+    dataJson = json.loads(dataString)
+
+    # is stunder number is not match grades number means some column is empty
+    if len(dataJson["identity"]) != len(dataJson["grades"]):
+        return "column lost"
+
+    # save user as dict
+    userDict = {}
+    for i in range(0, len(dataJson["identity"])):
+        userDict[dataJson["identity"][str(i)]] = dataJson["grades"][str(i)]
+
+    # sort user as tuple
+    userArray = sorted(userDict.items(), key=lambda x: x[1])
+
+    # if user total % 3 == 2 let low group and mid group +1 if % 3 ==1 let low group +1
+    userNum = len(userArray) // 3
+    if len(userArray) % 3 == 2:
+        lowNum = userNum + 1
+        midNum = userNum + 1
+    elif len(userArray) % 3 == 1:
+        lowNum = userNum + 1
+        midNum = userNum
+    else:
+        lowNum = userNum
+        midNum = userNum
+
+    groupNum = groupCaculator(len(userArray))
+    groupList = {}
+
+    for i in range(len(userArray)):
+        if i < lowNum:
+            userDB.updateUser(
+                {"myquery": {"account": userArray[i][0]}, "newValues": {"$set": {"label": "low"}}})
+
+            if i >= groupNum:
+                groupList[0].append(userArray[i][0])
+            else:
+                groupList[i] = [userArray[i][0]]
+
+        elif i < lowNum + midNum:
+            userDB.updateUser(
+                {"myquery": {"account": userArray[i][0]}, "newValues": {"$set": {"label": "mid"}}})
+
+            if i - lowNum + 1 > groupNum:
+                groupList[1].append(userArray[i][0])
+            else:
+                groupList[i - lowNum].append(userArray[i][0])
+        else:
+            userDB.updateUser(
+                {"myquery": {"account": userArray[i][0]}, "newValues": {"$set": {"label": "high"}}})
+
+            groupList[i - lowNum - midNum].append(userArray[i][0])
+
+    updateGroup(groupList, className)
+
+    return "success"
     try:
         data = pd.read_excel(file, header=0, converters={'identity': str, 'grades': int})
         dataString = data.to_json()
